e101ffa1793b489d7df4304982ba6c2d9484a4f3
Kasper (git commits by Albert) <54741819+kasperireland@users.noreply.github.com>
2022-01-28 12:20:52 -0500

    Update AssemblyTitle & AssemblyProduct

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lain")]
+[assembly: AssemblyTitle("Loginwise")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lain")]
+[assembly: AssemblyProduct("Loginwise")]
 [assembly: AssemblyCopyright("deadmoon © ∞")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
